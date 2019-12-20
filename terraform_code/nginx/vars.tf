39bcdcd92d78150d9a29946a0d5da27b81f27cea
Dennis McCarthy <dmccuk@gmail.com>
2019-12-20 11:47:06 +0000

    Updated for v0.12

--- terraform_code/nginx/vars.tf
+++ terraform_code/nginx/vars.tf
@@ -1,21 +1,24 @@
 variable "AWS_REGION" {
-  default = "add-your-region"
+  default = "eu-west-1"
 }
+
 variable "AMIS" {
-  type = "map"
+  type = map(string)
   default = {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
-    eu-west-2 = "add-your-ami"
+    eu-west-1 = "ami-07bc06d193ddb69ed"
   }
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
   default = "sshkey"
 }
+
 variable "PATH_TO_PUBLIC_KEY" {
   default = "sshkey.pub"
 }
+
 variable "INSTANCE_USERNAME" {
   default = "ubuntu"
 }
