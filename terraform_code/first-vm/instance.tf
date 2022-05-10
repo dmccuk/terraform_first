3bb669437bbe00f1e64c2f3e3a403b8c827b1f78
Ubuntu <ubuntu@ip-172-31-8-219.eu-west-2.compute.internal>
2022-05-10 16:20:21 +0000

    updates for v1.1

--- terraform_code/first-vm/instance.tf
+++ terraform_code/first-vm/instance.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region     = "add-you-region"
+  region     = "eu-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "add-your-ami"
+resource "aws_instance" "LondonIAC_example_01" {
+  ami           = "ADD-YOUR-AMI-ID"
   instance_type = "t2.micro"
 }
--- terraform_code/nginx/vars.tf
+++ terraform_code/nginx/vars.tf
@@ -1,13 +1,13 @@
 variable "AWS_REGION" {
-  default = "add-your-region"
+  default = "eu-west-2"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "add-your-ami"
+    us-east-1 = "ami-13be557ekjksjskjs"
+    us-west-2 = "ami-06b94666djdhs9shh"
+    eu-west-2 = "ADD-YOUR-AMI_ID"
   }
 }
 
